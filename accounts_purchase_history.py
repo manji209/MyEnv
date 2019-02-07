87ba0eac67bca9e3e40fc785024a2eef64f1b59b
Dinh Ho <daniel25th@gmail.com>
2019-02-06 16:57:22 -0800

    added order_entry.py

--- accounts_purchase_history.py
+++ accounts_purchase_history.py
@@ -10,11 +10,20 @@ cur = conn.cursor()
 
 #sqlstring = 'SELECT invc_no, invc_dat, cust_no, item_no, invc_or_cred, desc_1, desc_2, qty, prc, ext_prc, ord_no, sls_rep FROM dbo.IHSLIN00 WHERE year(invc_dat) >= 2018 AND cust_no=?'
 #sqlstring = 'SELECT hdr_cust_no, invc_or_cred, tot_sls FROM dbo.IHSHDR00'
-sqlstring = """SELECT DISTINCT hdr_cust_no, SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) AS total_sales, 
-                SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) AS total_credits, max(hdr_invc_dat) as MaxDateTime 
+
+sqlstring = """SELECT DISTINCT hdr_cust_no,
+                SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) AS total_net_sales,  
+                SUM(CASE WHEN invc_or_cred =? AND (DATEPART(YEAR, hdr_invc_dat) BETWEEN ? AND ?) THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? AND (DATEPART(YEAR, hdr_invc_dat) BETWEEN ? AND ?)
+                    THEN tot_sls ELSE 0 END) AS net_sales_since_2015, 
+                max(hdr_invc_dat) as last_inv_date,
+                SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) AS yr_2015,
+                SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) AS yr_2016,
+                SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) AS yr_2017,
+                SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? AND DATEPART(YEAR, hdr_invc_dat)=? THEN tot_sls ELSE 0 END) AS yr_2018 
                 FROM dbo.IHSHDR00
                 GROUP BY hdr_cust_no 
-                ORDER BY total_sales DESC"""
+                ORDER BY total_net_sales DESC"""
+
 
 '''
 sqlstring = """SELECT DISTINCT hdr_cust_no, SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) - SUM(CASE WHEN invc_or_cred =? THEN tot_sls ELSE 0 END) AS net_sales 
@@ -25,13 +34,18 @@ sqlstring = """SELECT DISTINCT hdr_cust_no, SUM(CASE WHEN invc_or_cred =? THEN t
 
 inv = 'I'
 credit = 'C'
-df = pd.read_sql(sqlstring, conn, params=(inv, credit))
+y1 = 2015
+y2 = 2016
+y3 = 2017
+y4 = 2018
 
+df = pd.read_sql(sqlstring, conn, params=(inv, credit, inv, y1, y4, credit, y1, y4, inv, y1, credit, y1, inv, y2, credit, y2, inv, y3, credit, y3, inv, y4, credit, y4))
+df['hdr_cust_no'] = df['hdr_cust_no'].str.strip()
 #df_ext_prc = df.pivot_table(values=['ext_prc', 'qty'], index=['item_no'], aggfunc={'ext_prc':np.sum, 'qty':np.sum})
 #df_ext_prc.sort_values(by="ext_prc", ascending=False, inplace=True)
 
 # Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter('Out/accounts_pivot_out2.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('Out/accounts_pivot_out5.xlsx', engine='xlsxwriter')
 
 df.to_excel(writer, sheet_name='Raw Data', index=False)
 # Write dataframe to Excel
---
+++ order_entry.py
@@ -0,0 +1,140 @@
+import pyodbc
+import xlrd
+import pandas as pd
+import numpy as np
+
+
+# Connect to SQL Server and set cursor
+conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=LALUCKYSERVER\SQLEXPRESS;DATABASE=pbsdataDEMO;UID=pbssqluser;PWD=Admin11')
+
+cur = conn.cursor()
+
+# read data
+#data = pd.read_excel('Data/import_order_entery_template.xlsx')
+
+# Open the workbook and define the worksheet
+book = xlrd.open_workbook("Data/import_order_entery_template.xlsx")
+sheet = book.sheet_by_name("Sheet1")
+
+
+query = """INSERT INTO [dbo].[LINITM00] (
+            [ord_no]
+           ,[seq_no]
+           ,[item_no]
+           ,[item_desc_1]
+           ,[desc_1]
+           ,[desc_2]
+           ,[qty_ord]
+           ,[ord_unit_of_meas]
+           ,[qty_to_shp]
+           ,[qty_bo]
+           ,[qty_ret_inv]
+           ,[scrap_svc_acctpc1]
+           ,[scrap_svc_acctpc2]
+           ,[scrap_svc_acctmain]
+           ,[scrap_svc_acctsub]
+           ,[bo_cod]
+           ,[unit_prc]
+           ,[conv_fac]
+           ,[prc_unit_of_meas]
+           ,[prc_unit_used]
+           ,[disc_amt]
+           ,[lin_item_acct_pc1]
+           ,[lin_item_acct_pc2]
+           ,[commis_pct]
+           ,[commis_amt]
+           ,[unit_cost]
+           ,[txbl_cod_1]
+           ,[txbl_cod_2]
+           ,[txbl_cod_3]
+           ,[txbl_cod_4]
+           ,[txbl_cod_5]
+           ,[sel_cod]
+           ,[sell_unit]
+           ,[qty_conv_fac]
+           ,[track_flg]
+           ,[track_qty]
+           ,[weight]
+           ,[negotiated_cost]
+           ,[estimated_cost]
+           ,[ex_txbl_amt]
+           ,[tax_amt]
+           ,[qty_scanned]
+           ,[po_lin_po_no]
+           ,[po_lin_lin_no]
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+           ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+
+# grab existing row count in the database for validation later
+cur.execute("SELECT count(*) FROM dbo.LINITM00")
+before_import = cur.fetchone()
+
+for r in range(1, sheet.nrows):
+    ord_no = sheet.cell(r,0).value
+    seq_no = sheet.cell(r,1).value
+    item_no = sheet.cell(r,2).value
+    item_desc_1 = sheet.cell(r,3).value
+    desc_1 = sheet.cell(r,4).value
+    desc_2 = sheet.cell(r,5).value
+    qty_ord = sheet.cell(r,6).value
+    ord_unit_of_meas = sheet.cell(r,7).value
+    qty_to_shp = sheet.cell(r,8).value
+    qty_bo = sheet.cell(r,9).value
+    qty_ret_inv = sheet.cell(r,10).value
+    scrap_svc_acctpc1 = sheet.cell(r, 11).value
+    scrap_svc_acctpc2 = sheet.cell(r, 12).value
+    scrap_svc_acctmain = sheet.cell(r, 13).value
+    scrap_svc_acctsub = sheet.cell(r, 14).value
+    bo_cod = sheet.cell(r, 15).value
+    unit_prc = sheet.cell(r, 16).value
+    conv_fac = sheet.cell(r, 17).value
+    prc_unit_of_meas = sheet.cell(r, 18).value
+    prc_unit_used = sheet.cell(r, 19).value
+    disc_amt = sheet.cell(r, 20).value
+    lin_item_acct_pc1 = sheet.cell(r, 21).value
+    lin_item_acct_pc2 = sheet.cell(r, 22).value
+    commis_pct = sheet.cell(r, 23).value
+    commis_amt = sheet.cell(r, 24).value
+    unit_cost = sheet.cell(r, 25).value
+    txbl_cod_1 = sheet.cell(r, 26).value
+    txbl_cod_2 = sheet.cell(r, 27).value
+    txbl_cod_3 = sheet.cell(r, 28).value
+    txbl_cod_4 = sheet.cell(r, 29).value
+    txbl_cod_5 = sheet.cell(r, 30).value
+    sel_cod = sheet.cell(r, 31).value
+    sell_unit = sheet.cell(r, 32).value
+    qty_conv_fac = sheet.cell(r, 33).value
+    track_flg = sheet.cell(r, 34).value
+    track_qty = sheet.cell(r, 35).value
+    weight = sheet.cell(r, 36).value
+    negotiated_cost = sheet.cell(r, 37).value
+    estimated_cost = sheet.cell(r, 38).value
+    ex_txbl_amt = sheet.cell(r, 39).value
+    tax_amt = sheet.cell(r, 40).value
+    qty_scanned = sheet.cell(r, 41).value
+    po_lin_po_no = sheet.cell(r, 42).value
+    po_lin_lin_no = sheet.cell(r, 43).value
+
+    # Assign values from each row
+    values = (ord_no, seq_no, item_no, item_desc_1, desc_1, desc_2, qty_ord, ord_unit_of_meas, qty_to_shp, qty_bo,
+              qty_ret_inv, scrap_svc_acctpc1, scrap_svc_acctpc2, scrap_svc_acctmain, scrap_svc_acctsub, bo_cod, unit_prc,
+              conv_fac, prc_unit_of_meas, prc_unit_used, disc_amt, lin_item_acct_pc1, lin_item_acct_pc2, commis_pct, commis_amt,
+              unit_cost, txbl_cod_1, txbl_cod_2, txbl_cod_3, txbl_cod_4, txbl_cod_5, sel_cod, sell_unit, qty_conv_fac, track_flg,
+              track_qty, weight, negotiated_cost, estimated_cost, ex_txbl_amt, tax_amt, qty_scanned, po_lin_po_no, po_lin_lin_no)
+
+    # Execute sql Query
+    cur.execute(query, values)
+
+
+
+# Commit the transaction
+conn.commit()
+
+# If you want to check if all rows are imported
+cur.execute("SELECT count(*) FROM dbo.LINITM00")
+result = cur.fetchone()
+
+print((result[0] - before_import[0]))  # should be True
+
+# Close the database connection
+conn.close()
